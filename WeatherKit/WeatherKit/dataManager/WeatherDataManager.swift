8080f746ec4dce30f913586fe6a7393f9353755a
Lanouari Issam <issam.lanouari@gmail.com>
2020-08-14 22:04:42 +0200

    [ADD] Singleton weather data manager containing persistentContainer of WeatherModel

---
+++ WeatherKit/WeatherKit/dataManager/WeatherDataManager.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherDataManager.swift
+//  WeatherKit
+//
+//  Created by Issam Lanouari on 14/08/2020.
+//  Copyright © 2020 Issam Lanouari. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class WeatherDataManager {
+    
+    static let shared = WeatherDataManager()
+    
+    private let identifier: String = "com.lanouari.WeatherKit"
+    private let model: String = "WeatherModel"
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let messageKitBundle = Bundle(identifier: self.identifier)
+        let modelURL = messageKitBundle!.url(forResource: self.model, withExtension: "momd")!
+        let managedObjectModel =  NSManagedObjectModel(contentsOf: modelURL)
+        let container = NSPersistentContainer(name: self.model, managedObjectModel: managedObjectModel!)
+        container.loadPersistentStores { (storeDescription, error) in
+            if let err = error {
+                fatalError("Loading of store failed with error:\(err)")
+            }
+        }
+        return container
+    }()
+}
