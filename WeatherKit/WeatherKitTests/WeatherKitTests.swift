41bff6a891e799a9413d3f3427ccb7baba36dd42
Lanouari Issam <issam.lanouari@gmail.com>
2020-08-14 23:47:45 +0200

    [ADD] Weather info services tests

--- WeatherKit/WeatherKitTests/WeatherKitTests.swift
+++ WeatherKit/WeatherKitTests/WeatherKitTests.swift
@@ -7,28 +7,51 @@
 //
 
 import XCTest
+import CoreData
 @testable import WeatherKit
 
-class WeatherKitTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class NetworkManagerTests: XCTestCase {
+    lazy var mockPersistentContainer: NSPersistentContainer = {
+        let messageKitBundle = Bundle(identifier: "com.lanouari.WeatherKit")
+        let modelURL = messageKitBundle!.url(forResource: "WeatherModel", withExtension: "momd")!
+        let managedObjectModel =  NSManagedObjectModel(contentsOf: modelURL)
+        let container = NSPersistentContainer(name: "CityData", managedObjectModel: managedObjectModel!)
+        let description = NSPersistentStoreDescription()
+        
+        description.type = NSInMemoryStoreType
+        description.shouldAddStoreAsynchronously = false // Make it simpler in test env
+        container.persistentStoreDescriptions = [description]
+        container.loadPersistentStores { (description, error) in
+            // Check if the data store is in memory
+            precondition( description.type == NSInMemoryStoreType )
+            // Check if creating container wrong
+            if let error = error {
+                fatalError("Create an in-mem coordinator failed \(error)")
+            }
+        }
+        return container
+    }()
+    func test_getCity_service() {
+        let oneCallService = WeatherInfoService(mockPersistentContainer)
+        oneCallService.createNewCity(name: "TestCity", longitude: 20.0, latitude: 30.4) {   result in
+            switch result {
+            case let .failure(error):
+                XCTFail(error.localizedDescription)
+            default:
+                break
+            }
         }
     }
-
+    func test_getAllCities_service() {
+        let oneCallService = WeatherInfoService(mockPersistentContainer)
+        oneCallService.createNewCity(name: "TestCity", longitude: 20.0, latitude: 30.4, completion: { _ in })
+        oneCallService.getAllCities() { result in
+            switch result {
+            case let .success(citiesnames):
+                XCTAssertEqual(1, citiesnames.count)
+            case let .failure(error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+    }
 }
