61190293d53f0d1bfc61d108016ac75f777c3495
Lanouari Issam <issam.lanouari@gmail.com>
2020-08-15 15:05:30 +0200

    [ADD] City details interactor to manage interaction with service

--- WeatherApp/scenes/cityDetails/CityDetailsInteractor.swift
+++ WeatherApp/scenes/cityDetails/CityDetailsInteractor.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import WeatherKit
+
+protocol CityWeatherDetailsUseCase {
+  var viewModel: Observable<CityDetailsDisplayContext> { get }
+  func getCityWeatherDetails()
+  
+}
+
+class CityWeatherDetailInteractor: CityWeatherDetailsUseCase {
+  
+  var viewModel: Observable<CityDetailsDisplayContext>
+  private let weatherInfoService: WeatherInfoService
+  private let cityName: String
+  
+  internal init(weatherInfoService: WeatherInfoService, cityName: String) {
+    self.cityName = cityName
+    self.weatherInfoService = weatherInfoService
+    self.viewModel = Observable<CityDetailsDisplayContext>(value: .loading(cityName))
+  }
+  
+  func getCityWeatherDetails() {
+    weatherInfoService.getCityWeatherInformations(name: cityName) { [weak self] result in
+      switch result {
+      case .failure(_):
+        self?.viewModel.value = .noData
+      case .local(let data):
+        self?.viewModel.value = .success(CityDetailsViewModel(city: data, isRemote: false))
+      case .remote(let data):
+        self?.viewModel.value  = .success(CityDetailsViewModel(city: data, isRemote: true))
+      }
+    }
+  }
+}
