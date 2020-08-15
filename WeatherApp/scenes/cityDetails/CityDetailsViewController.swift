6289b030a9dec73c144bb4b672145da4da3be575
Lanouari Issam <issam.lanouari@gmail.com>
2020-08-15 14:58:17 +0200

    [UPDATE] City details viewController with design components

--- WeatherApp/scenes/cityDetails/CityDetailsViewController.swift
+++ WeatherApp/scenes/cityDetails/CityDetailsViewController.swift
@@ -9,22 +9,40 @@
 import UIKit
 
 class CityDetailsViewController: UIViewController {
-
+    
+    // MARK :- Outlets
+    @IBOutlet private weak var latitudeLabel: UILabel!
+    @IBOutlet private weak var longitudeLabel: UILabel!
+    @IBOutlet private weak var timeZoneLabel: UILabel!
+    @IBOutlet private weak var currentTimeLabel: UILabel!
+    @IBOutlet private weak var temperatureLabel: UILabel!
+    @IBOutlet private weak var feelsLikeLabel: UILabel!
+    @IBOutlet private weak var pressureLabel: UILabel!
+    @IBOutlet private weak var humidityLabel: UILabel!
+    @IBOutlet private weak var windSpeedLabel: UILabel!
+    @IBOutlet private weak var windDegreeLabel: UILabel!
+    @IBOutlet private weak var weatherMainLabel: UILabel!
+    @IBOutlet private weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet private weak var dataProvenanceLabel: UILabel!
+    @IBOutlet private weak var weatherIconLabel: UIImageView!
+    @IBOutlet private weak var contentScrollView: UIScrollView!
+    @IBOutlet private weak var noDataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
